74b59db1e0d398211343d730f0691e54940622eb
Daibon <saovayem36@gmail.com>
2026-02-04 20:43:50 +0700

    Upload Laravel project

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Trang Liên Hệ</h1>
+    <p>Email: saovayem36@gmail.com</p>
+    <p>Phone: 0123456789</p>
+</body>
+</html>
